adf85caf4d26a61bcae3ff10df80fe7909751753
Leland Rowley <lelandrowley@gmail.com>
2012-10-18 15:13:20 -0500

    A Few Things to Try, first bullet

---
+++ learn_to_program/5.6.rb
@@ -0,0 +1,6 @@
+puts "Hello! What is your first name?"
+first_name = gets.chomp
+puts "Great! I like the name #{first_name}."
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Nice to meet you #{first_name} #{last_name}!"
