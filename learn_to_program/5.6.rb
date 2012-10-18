27ca5142fdde5d9c77c0f29d6cb999ad74c882a2
Leland Rowley <lelandrowley@gmail.com>
2012-10-18 16:28:34 -0500

    5.6 challenge

--- learn_to_program/5.6.rb
+++ learn_to_program/5.6.rb
@@ -1,6 +1,11 @@
-puts "Hello! What is your first name?"
-first_name = gets.chomp
-puts "Great! I like the name #{first_name}."
-puts "What is your last name?"
-last_name = gets.chomp
-puts "Nice to meet you #{first_name} #{last_name}!"
+# puts "Hello! What is your first name?"
+# first_name = gets.chomp
+# puts "Great! I like the name #{first_name}."
+# puts "What is your last name?"
+# last_name = gets.chomp
+# puts "Nice to meet you #{first_name} #{last_name}!"
+
+
+ puts "What is your most favoritest number?"
+ og_favorite_num = gets.chomp
+ puts "Ooooh! That is a great number! You know what other number is pretty amazing? #{og_favorite_num.to_i + 1}. Everyone likes #{og_favorite_num.to_i + 1}! "
