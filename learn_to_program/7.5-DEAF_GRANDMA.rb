67f2b53639b1c6357d5f054046448a11d1565275
Leland Rowley <lelandrowley@gmail.com>
2012-10-20 18:37:43 -0500

    Final product. Can't think of a way to refactor down any more (someday I might be able to?). I moved the if chat == bye up above the other if statement to fix my problem of printing the second if statement more elegantly than the && != BYE

--- learn_to_program/7.5-DEAF_GRANDMA.rb
+++ learn_to_program/7.5-DEAF_GRANDMA.rb
@@ -1,18 +1,20 @@
 while true
-  date = rand(21) + 1930
+  
   puts "Hi grandchild. I am your grandmother. You have a question?"
   chat = gets.chomp
 
-  if chat == chat.upcase && chat != "BYE"
-    puts "NO, NOT SINCE #{date}"
+  if chat == "BYE"
+      puts "You want to leave your poor old granny? Ok... well bundle up!"
+      puts " "
+      break
+  end
+    
+  if chat == chat.upcase
+    puts "NO, NOT SINCE #{rand(21) + 1930}"
   else
    puts "HUH?! SPEAK UP SONNY!"
   end
   
   puts " "
-  
-   if chat == "BYE"
-      break
-    end
 
 end
