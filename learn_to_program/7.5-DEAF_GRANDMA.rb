96af1618bc7266988a6625b0dc45f5bb67a25fd8
Leland Rowley <lelandrowley@gmail.com>
2012-10-20 17:43:11 -0500

    getting closer. figuring out the looping syntax.

--- learn_to_program/7.5-DEAF_GRANDMA.rb
+++ learn_to_program/7.5-DEAF_GRANDMA.rb
@@ -1,10 +1,40 @@
-puts "Hi grandchild. I am your grandmother."
-grandchild = gets.chomp
+while true
+date = rand(21) + 1930
+puts "Hi grandchild. I am your grandmother. You have a question?"
+chat = gets.chomp
 
-# if grandchild == all_caps
-# #GRANDMA "HEARS" AND RESPONDS WITH "NO, NOT SINCE #{random_date_between_1930&1950}"
-# end
+  if chat == chat.upcase
+  # GRANDMA "HEARS" AND RESPONDS WITH "NO, NOT SINCE #{random_date_between_1930&1950}"
+    puts "NO, NOT SINCE #{date}"
+  else
+   puts "HUH?! SPEAK UP SONNY!"
 
-else
-  puts "HUH?! SPEAK UP SONNY!"
+  # else chat == "BYE"
+  #    break
+  end
+
 end
+
+
+
+
+# say = 'hi grammy'
+#   while say != 'BYE'
+#     say = gets.chomp
+#   while say != say.upcase
+#       puts 'HUH? SPEAK UP SONNY!'
+#       say = gets.chomp
+# end
+#   year = rand(21) + 1930
+#   puts 'NO, NOT SINCE ' + year.to_s
+# end
+# 
+# 
+# 
+# str == str.upcase
+# 
+# E.g:
+# str = "DOG"
+# str == str.upcase  # true
+# str = "cat"
+# str == str.upcase  # false
