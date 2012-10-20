c9f48770542b1bf7ef74d99f5772d0ccf6feaf1b
Leland Rowley <lelandrowley@gmail.com>
2012-10-20 18:01:46 -0500

    got it working, but adding the || to see if I can get it to not say the if statement before ending.

--- learn_to_program/7.5-DEAF_GRANDMA.rb
+++ learn_to_program/7.5-DEAF_GRANDMA.rb
@@ -1,19 +1,18 @@
 while true
-date = rand(21) + 1930
-puts "Hi grandchild. I am your grandmother. You have a question?"
-chat = gets.chomp
+  date = rand(21) + 1930
+  puts "Hi grandchild. I am your grandmother. You have a question?"
+  chat = gets.chomp
 
-  if chat == chat.upcase
-  # GRANDMA "HEARS" AND RESPONDS WITH "NO, NOT SINCE #{random_date_between_1930&1950}"
+  if chat == chat.upcase && chat != "BYE"
     puts "NO, NOT SINCE #{date}"
   else
    puts "HUH?! SPEAK UP SONNY!"
-
-  # else chat == "BYE"
-  #    break
   end
+  
+  puts " "
+  
+   if chat == "BYE"
+      break
+    end
 
 end
-
-
- 
