f84a074f0e886467bd93735af229f28df3aaf402
Leland Rowley <lelandrowley@gmail.com>
2012-10-20 17:21:16 -0500

    Working on the deaf grandma problem. Just thinking through it here and setting out the problem...

---
+++ learn_to_program/7.5-DEAF_GRANDMA.rb
@@ -0,0 +1,10 @@
+puts "Hi grandchild. I am your grandmother."
+grandchild = gets.chomp
+
+# if grandchild == all_caps
+# #GRANDMA "HEARS" AND RESPONDS WITH "NO, NOT SINCE #{random_date_between_1930&1950}"
+# end
+
+else
+  puts "HUH?! SPEAK UP SONNY!"
+end
