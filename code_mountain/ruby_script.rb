2842162aa5dd06879f6faad678f9d3068bb85afe
Leland Rowley <lelandrowley@gmail.com>
2012-10-22 10:40:42 -0500

    Added HTML tags to manually export the html. Inelegant way to do this, but this is our assignment :)

--- code_mountain/ruby_script.rb
+++ code_mountain/ruby_script.rb
@@ -1,11 +1,24 @@
- puts "Leland's List of Restaurants".upcase
-
+puts "<html>"
+  puts "<h1>"
+    puts "Leland's List of Restaurants".upcase 
+  puts "</h1>"
+  
 favorite_restaurants = [
   {:name => 'terra Mia', :genre => 'italian', :cost => '$$'},
   {:name => 'cubby\'s', :genre => 'american', :cost => '$$'},
   {:name => 'bombay House', :genre => 'indian', :cost => '$$'}
    ]
+  
+   puts  "<body>"
+       
+       favorite_restaurants.each do |display|
+         puts "<ul>"
+         puts "<li>"
+         puts "#{display[:name].capitalize} is my favorite #{display[:genre].capitalize} restaurant. (#{display[:cost]})" 
+         puts "</li>"
+         puts "</ul>"
+       end
    
-   favorite_restaurants.each do |display|
-     puts "#{display[:name].capitalize} is my favorite #{display[:genre].capitalize} restaurant. (#{display[:cost]})" 
-   end
+    puts "</body>"
+   
+puts "</html>"
